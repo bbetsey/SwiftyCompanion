c9f78eabf4f57997cf86a9a3703fd89ab4d63017
Byrd Betsey <bbetsey12@gmail.com>
2022-10-13 03:27:38 +0400

    feat(app): first version finished

---
+++ SwiftyCompanion/Controllers/MainNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  MainNavigationController.swift
+//  SwiftyCompanion
+//
+//  Created by Антон Тропин on 09.10.2022.
+//
+
+import UIKit
+import AuthenticationServices
+
+class MainNavigationController: UINavigationController {
+
+}
